cfd2b16fd508d0cef44867b7f368bf504a2a28f9
eliffT <elifftasdemirr@gmail.com>
2025-08-23 21:12:06 +0300

    [SP-2] - Postman Installation and Initial Testing

---
+++ src/main/java/com/elif/spring/UserController.java
@@ -0,0 +1,24 @@
+package com.elif.spring;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping("/{id}")
+    public Map<String, Object> getUser(@PathVariable Long id) {
+        return Map.of(
+                "id", id,
+                "name", "Test User",
+                "email", "test@example.com"
+        );
+    }
+
+
+}
